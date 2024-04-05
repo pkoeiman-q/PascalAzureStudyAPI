299acaafcdabfe3491e634b0514a8ad968d00034
Pascal Koeiman <pkoeiman@quintor.nl>
2024-04-05 10:56:40 +0200

    Update program.cs and adjust weatherforecastcontroller route to work with Azure

--- PascalAzureStudyAPI/Controllers/WeatherForecastController.cs
+++ PascalAzureStudyAPI/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PascalAzureStudyAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
