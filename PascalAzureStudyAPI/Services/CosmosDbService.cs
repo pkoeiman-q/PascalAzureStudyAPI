4e3cd2249e96d0fb856b939c7a940416c00be57d
Pascal Koeiman <pkoeiman@quintor.nl>
2024-04-08 10:15:11 +0200

    Add CosmosDb service (need to rename later to PortfolioService probably)

---
+++ PascalAzureStudyAPI/Services/CosmosDbService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.Cosmos;
+using PascalAzureStudyAPI.Models;
+
+namespace PascalAzureStudyAPI.Services
+{
+    public class CosmosDbService : ICosmosDbService
+    {
+        private Container _container;
+
+        public CosmosDbService(
+            CosmosClient cosmosDbClient,
+            string databaseName,
+            string containerName)
+        {
+            _container = cosmosDbClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddAsync(Portfolio portfolio)
+        {
+            await _container.CreateItemAsync(portfolio, new PartitionKey(portfolio.PortfolioId));
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _container.DeleteItemAsync<Portfolio>(id, new PartitionKey(id));
+        }
+
+        public async Task<Portfolio> GetAsync(string id)
+        {
+            try
+            {
+                var response = await _container.ReadItemAsync<Portfolio>(id, new PartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException) //For handling item not found and other exceptions
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<Portfolio>> GetMultipleAsync(string queryString)
+        {
+            var query = _container.GetItemQueryIterator<Portfolio>(new QueryDefinition(queryString));
+
+            var results = new List<Portfolio>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateAsync(string id, Portfolio portfolio)
+        {
+            await _container.UpsertItemAsync(portfolio, new PartitionKey(id));
+        }
+    }
+}
---
+++ PascalAzureStudyAPI/Services/ICosmosDbService.cs
@@ -0,0 +1,13 @@
+﻿using PascalAzureStudyAPI.Models;
+
+namespace PascalAzureStudyAPI.Services
+{
+    public interface ICosmosDbService1
+    {
+        Task AddAsync(Portfolio portfolio);
+        Task DeleteAsync(string id);
+        Task<Portfolio> GetAsync(string id);
+        Task<IEnumerable<Portfolio>> GetMultipleAsync(string queryString);
+        Task UpdateAsync(string id, Portfolio portfolio);
+    }
+}
