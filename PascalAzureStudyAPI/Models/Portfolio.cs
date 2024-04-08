542b6eef0d92341af8997ac09b271a0c21065430
Pascal Koeiman <pkoeiman@quintor.nl>
2024-04-08 10:14:48 +0200

    Add model for Portfolio

---
+++ PascalAzureStudyAPI/Models/Portfolio.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace PascalAzureStudyAPI.Models
+{
+    public class Portfolio
+    {
+        [JsonProperty(PropertyName = "portfolioId")]
+        public string PortfolioId { get; set; }
+
+        [JsonProperty(PropertyName = "userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
