f76355ee710c22738b5a8d5267f331cc095de1e3
Pascal Koeiman <pkoeiman@quintor.nl>
2024-04-22 17:07:00 +0200

    Adjust portfolio model and add workexperience model

--- PascalAzureStudyAPI/Models/Portfolio.cs
+++ PascalAzureStudyAPI/Models/Portfolio.cs
@@ -8,13 +8,12 @@ namespace PascalAzureStudyAPI.Models
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
 
+        // 'portfolioId' is the partition key within azure cosmos DB
+        // When creating a portfolio, set this ID to be equal to the user ID
         [JsonProperty(PropertyName = "portfolioId")]
         public string PortfolioId { get; set; }
 
-        [JsonProperty(PropertyName = "userId")]
-        public string UserId { get; set; }
-
-        [JsonProperty(PropertyName = "description")]
-        public string Description { get; set; }
+        [JsonProperty(PropertyName = "experiences")]
+        public IEnumerable<WorkExperience> Experiences { get; set; }
     }
 }
---
+++ PascalAzureStudyAPI/Models/WorkExperience.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace PascalAzureStudyAPI.Models
+{
+    public class WorkExperience
+    {
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+    }
+}
